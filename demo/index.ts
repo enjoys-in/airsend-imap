e11468c8291dedbdd8f520a50e41b0297869b0f0
Mullayam <mullayam06@gmail.com>
2025-10-11 21:24:01 +0530

    IMAP Implemented

--- demo/index.ts
+++ demo/index.ts
@@ -1,31 +1,31 @@
 import Imap from 'node-imap'
 
 var imap = new Imap({
-    user: 'mygmailname',
-    password: 'mygmailpassword',
-    host: 'localhost',
-    port: 143,
-    tls: false,
-    debug(info) {
-        console.log(info)
-    }, 
+  user: "mullayam06@airsend.in",
+  password: "4de75c41c9e04bd2",
+  host: 'localhost',
+  port: 143,
+  tls: false,
+  debug(info) {
+    console.log(info)
+  },
 
 });
 imap.once('ready', function () {
-    imap.openBox('INBOX', false, function (err, box) {
-        if (err) throw err;
-        var f = box.messages.total;
-        console.log(f + ' messages');
-        imap.end();
-    });
+  imap.openBox('INBOX', false, function (err, box) {
+    if (err) throw err;
+    var f = box.messages.total;
+    console.log(f + ' messages');
+    imap.end();
+  });
 });
 
-imap.once('error', function(err) {
+imap.once('error', function (err) {
   console.log(err);
 });
- 
-imap.once('end', function() {
+
+imap.once('end', function () {
   console.log('Connection ended');
 });
- 
+
 imap.connect();
